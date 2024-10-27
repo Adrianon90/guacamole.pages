935f515ecb908cf8c8b9002ea323802ee0ec4b4f
Adriano <158053440+GuacamoleRockstar@users.noreply.github.com>
2024-10-27 16:23:26 +0000

    Initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Bem-vindo ao meu site React no GitHub Pages!</h1>
+      <p>Esta é minha primeira página React no GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
+
+export default App;
